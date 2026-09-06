c17654d3568330c9526e67ff5217f55820733bea
Anatoly Chernov <chertoly@gmail.com>
2026-09-06 20:05:24 +0300

    Extract C.lib

--- src.source/bin/tmux.run.cr
+++ src.source/bin/tmux.run.cr
@@ -1,21 +1,12 @@
 require "yaml"
 require "process"
 
+require "../c"
 require "../command_result"
 require "../pty_command"
 require "../tmux"
 require "../runner"
 
-lib C
-  fun openpty(amaster : Int32*, aslave : Int32*, name : UInt8*, termp : Void*, winp : Void*) : Int32
-  fun login_tty(fd : Int32) : Int32
-  fun fork : Int32
-  fun _exit(status : Int32) : NoReturn
-end
-
-# =============================================================================
-# 4. ENTRY POINT
-# =============================================================================
 if ARGV.size < 2
   puts "Usage: tmux.run <output_log.yaml> \"command 1\" \"command 2\" ..."
   exit 1
---
+++ src/c.cr
@@ -0,0 +1,6 @@
+lib C
+  fun openpty(amaster : Int32*, aslave : Int32*, name : UInt8*, termp : Void*, winp : Void*) : Int32
+  fun login_tty(fd : Int32) : Int32
+  fun fork : Int32
+  fun _exit(status : Int32) : NoReturn
+end
