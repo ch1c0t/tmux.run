c5f8aacc4ffc7f623915c447c7c259c9659f378d
Anatoly Chernov <chertoly@gmail.com>
2026-09-11 18:40:48 +0300

    Add a help message

--- src.source/bin/tmux.run.cr
+++ src.source/bin/tmux.run.cr
@@ -11,7 +11,7 @@ require "../tmux"
 require "../runner"
 
 if ARGV.size < 2
-  puts "Usage: tmux.run <output_log.yaml> \"command 1\" \"command 2\" ..."
+  puts "Usage: tmux.run \"command 1\" \"command 2\" ..."
   exit 1
 end
 
